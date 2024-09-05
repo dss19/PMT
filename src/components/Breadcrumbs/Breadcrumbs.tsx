5a08f7582be6bc3da29175291438426b5de59884
dss19 <you@example.com>
2024-09-05 22:47:02 +0300

    crunbs test

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React from 'react';
 import './breadcrumbs.css';
+import { Link, useLocation } from 'react-router-dom';
 
-const Breadcrumbs: React.FC = () => {
+const breadcrumbMap: Record<string, string> = {
+    '/': 'Главная',
+    '/about': 'О компании',
+    '/news': 'Новости',
+    '/payment': 'Оплата и доставка',
+    '/contacts': 'Контакты',
+    '/catalog': 'Каталог'
+};
+
+const BreadCrumbs: React.FC = () => {
+    const location = useLocation();
+    const pathArray = location.pathname.split('/').filter(Boolean);
+
+    const generateBreadcrumb = (pathArray: string[]) => {
+        let path = '';
+        return pathArray.map((part, index) => {
+            path += `/${part}`;
+            const isLast = index === pathArray.length - 1;
+
+            if (breadcrumbMap[`/${part}`]) {
+                return !isLast ? (
+                    <React.Fragment key={index}>
+                        <Link to={path}>{breadcrumbMap[`/${part}`]}</Link>&nbsp;—&nbsp;
+                    </React.Fragment>
+                ) : (
+                    breadcrumbMap[`/${part}`]
+                );
+            }
+
+            const name = decodeURIComponent(part);
+            return !isLast ? (
+                <React.Fragment key={index}>
+                    <Link to={path}>{name}</Link>&nbsp;—&nbsp; 
+                </React.Fragment>
+            ) : (
+                name
+            );
+        });
+    };
 
     return (
-        <nav className="breadcrumbs">
-            Главная - О компании
-        </nav>
-    )
-} 
+        <div className="breadcrumbs">
+            <Link to="/">Главная</Link>&nbsp;—&nbsp;{generateBreadcrumb(pathArray)}
+        </div>
+    );
+};
 
-export default Breadcrumbs;
-
+export default BreadCrumbs;
