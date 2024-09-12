aa4dfa0229b30f15767b5308134e067275ad35d6
dss19 <samoylov82@gmail.com>
2024-09-12 16:29:24 +0300

    добавил заголовок

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Section from '../../components/Section/Section';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 import Container from '../../components/Container/Container';
 import CartInner from './CartInner/CartInner';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 const Cart: React.FC = () => {
     
+    const [title] = useState<string>('Оформление заказа');
 
     return (
         <main id='page-cart' className='main'>
             <Section>
                 <Container>
                     <Breadcrumbs />
+                    <SectionTitle title={title} />
                 </Container>                
             </Section>
             <Section>
