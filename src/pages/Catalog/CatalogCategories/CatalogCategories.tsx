bdb739c71ab9b5be7973aa756376b50d94f5d320
dss19 <samoylov82@gmail.com>
2024-09-13 15:07:42 +0300

    сделал иконку для категории пневмоинструмента

--- src/pages/Catalog/CatalogCategories/CatalogCategories.tsx
+++ src/pages/Catalog/CatalogCategories/CatalogCategories.tsx
@@ -13,7 +13,8 @@ const CatalogCategories: React.FC<CategoryProps> = ({ category }) => {
         
     return (
         <Link to={ `/catalog/${category.slug}` } className="catalog-categories">
-            <h5>{ category.name }</h5>            
+            <h5 className="catalog-categories-name">{ category.name }</h5>  
+            <img className="catalog-categories-img" src="/images/category-icons/pneumo.svg" alt="" />          
         </Link>
     );
 };
