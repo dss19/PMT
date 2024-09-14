65a4814b86209109ac626fd21eb9706f733903eb
dss19 <you@example.com>
2024-09-14 14:23:43 +0300

    насторил запросы к серверу от категорий

--- src/models/ICaterogy.ts
+++ src/models/ICaterogy.ts
@@ -5,4 +5,5 @@ export default interface Category {
     name: string;
     slug: string;
     subCategories: ISubCategory[];
+    iconurl: string;
 }
--- src/pages/Catalog/CatalogCategories/CatalogCategories.tsx
+++ src/pages/Catalog/CatalogCategories/CatalogCategories.tsx
@@ -7,12 +7,15 @@ interface CategoryProps {
     category: ICategory
 }
 
-const CatalogCategories: React.FC<CategoryProps> = ({ category }) => {    
+const CatalogCategories: React.FC<CategoryProps> = ({ category }) => {  
+    
+    console.log(category);
+    
         
     return (
         <Link to={ `/catalog/${category.slug}` } className="catalog-categories">
             <h5 className="catalog-categories-name">{ category.name }</h5>  
-            <img className="catalog-categories-img" src="/images/category-icons/pneumo.svg" alt="" />          
+            <img className="catalog-categories-img" src={category.iconurl} alt={ category.name } />         
         </Link>
     );
 };
