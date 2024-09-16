a19d2a91714ffc53519f3790866ec418dc23688e
dss19 <samoylov82@gmail.com>
2024-09-16 10:41:55 +0300

    просто так

--- src/pages/Catalog/CatalogCategories/CatalogCategories.tsx
+++ src/pages/Catalog/CatalogCategories/CatalogCategories.tsx
@@ -12,7 +12,7 @@ const CatalogCategories: React.FC<CategoryProps> = ({ category }) => {
     return (
         <Link to={ `/catalog/${category.slug}` } className="catalog-categories">
             <h5 className="catalog-categories-name">{ category.name }</h5>  
-            <img className="catalog-categories-img" src="/images/category-icons/pneumo.svg" alt="" />          
+            <img className="catalog-categories-img" src="/images/category-icons/pneumo.svg" alt="пневмоинструмент" />          
         </Link>
     );
 };
